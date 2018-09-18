ed81c94153099fe281ec3fae3c96923fd51f9eb8
Александр <zalaw@bk.ru>
2018-09-18 17:49:17 +0300

    основные обработки

--- FoldSP/Form1.Designer.vb
+++ FoldSP/Form1.Designer.vb
@@ -22,9 +22,155 @@ Partial Class Form1
     'Не изменяйте ее в редакторе исходного кода.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.txb_NumSP = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ToolStrip1 = New System.Windows.Forms.ToolStrip()
+        Me.ToolStripDropDownButton1 = New System.Windows.Forms.ToolStripDropDownButton()
+        Me.ВыбратьПапкуToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ВыходToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripDropDownButton2 = New System.Windows.Forms.ToolStripDropDownButton()
+        Me.ИзменитьАдресаРабочихПапокToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripButton2 = New System.Windows.Forms.ToolStripButton()
+        Me.LinkLabelSP = New System.Windows.Forms.LinkLabel()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.FBD = New System.Windows.Forms.FolderBrowserDialog()
+        Me.ToolStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'txb_NumSP
+        '
+        Me.txb_NumSP.Location = New System.Drawing.Point(77, 32)
+        Me.txb_NumSP.Name = "txb_NumSP"
+        Me.txb_NumSP.Size = New System.Drawing.Size(100, 20)
+        Me.txb_NumSP.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Enabled = False
+        Me.Button1.Location = New System.Drawing.Point(183, 32)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(31, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Да"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 35)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(59, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Номер СП"
+        '
+        'ToolStrip1
+        '
+        Me.ToolStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripDropDownButton1, Me.ToolStripDropDownButton2, Me.ToolStripButton2})
+        Me.ToolStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.ToolStrip1.Name = "ToolStrip1"
+        Me.ToolStrip1.Size = New System.Drawing.Size(304, 25)
+        Me.ToolStrip1.TabIndex = 3
+        Me.ToolStrip1.Text = "ToolStrip1"
+        '
+        'ToolStripDropDownButton1
+        '
+        Me.ToolStripDropDownButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
+        Me.ToolStripDropDownButton1.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ВыбратьПапкуToolStripMenuItem, Me.ВыходToolStripMenuItem})
+        Me.ToolStripDropDownButton1.Image = CType(resources.GetObject("ToolStripDropDownButton1.Image"), System.Drawing.Image)
+        Me.ToolStripDropDownButton1.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.ToolStripDropDownButton1.Name = "ToolStripDropDownButton1"
+        Me.ToolStripDropDownButton1.Size = New System.Drawing.Size(62, 22)
+        Me.ToolStripDropDownButton1.Text = "Начало"
+        '
+        'ВыбратьПапкуToolStripMenuItem
+        '
+        Me.ВыбратьПапкуToolStripMenuItem.Name = "ВыбратьПапкуToolStripMenuItem"
+        Me.ВыбратьПапкуToolStripMenuItem.Size = New System.Drawing.Size(156, 22)
+        Me.ВыбратьПапкуToolStripMenuItem.Text = "Выбрать папку"
+        '
+        'ВыходToolStripMenuItem
+        '
+        Me.ВыходToolStripMenuItem.Name = "ВыходToolStripMenuItem"
+        Me.ВыходToolStripMenuItem.Size = New System.Drawing.Size(156, 22)
+        Me.ВыходToolStripMenuItem.Text = "Выход"
+        '
+        'ToolStripDropDownButton2
+        '
+        Me.ToolStripDropDownButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
+        Me.ToolStripDropDownButton2.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ИзменитьАдресаРабочихПапокToolStripMenuItem})
+        Me.ToolStripDropDownButton2.Image = CType(resources.GetObject("ToolStripDropDownButton2.Image"), System.Drawing.Image)
+        Me.ToolStripDropDownButton2.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.ToolStripDropDownButton2.Name = "ToolStripDropDownButton2"
+        Me.ToolStripDropDownButton2.Size = New System.Drawing.Size(80, 22)
+        Me.ToolStripDropDownButton2.Text = "Настройки"
+        '
+        'ИзменитьАдресаРабочихПапокToolStripMenuItem
+        '
+        Me.ИзменитьАдресаРабочихПапокToolStripMenuItem.Name = "ИзменитьАдресаРабочихПапокToolStripMenuItem"
+        Me.ИзменитьАдресаРабочихПапокToolStripMenuItem.Size = New System.Drawing.Size(253, 22)
+        Me.ИзменитьАдресаРабочихПапокToolStripMenuItem.Text = "Изменить адреса рабочих папок"
+        '
+        'ToolStripButton2
+        '
+        Me.ToolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
+        Me.ToolStripButton2.Image = CType(resources.GetObject("ToolStripButton2.Image"), System.Drawing.Image)
+        Me.ToolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.ToolStripButton2.Name = "ToolStripButton2"
+        Me.ToolStripButton2.Size = New System.Drawing.Size(57, 22)
+        Me.ToolStripButton2.Text = "Справка"
+        '
+        'LinkLabelSP
+        '
+        Me.LinkLabelSP.AutoSize = True
+        Me.LinkLabelSP.Location = New System.Drawing.Point(15, 63)
+        Me.LinkLabelSP.Name = "LinkLabelSP"
+        Me.LinkLabelSP.Size = New System.Drawing.Size(59, 13)
+        Me.LinkLabelSP.TabIndex = 4
+        Me.LinkLabelSP.TabStop = True
+        Me.LinkLabelSP.Text = "LinkLabel1"
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(335, 32)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(337, 368)
+        Me.ListBox1.TabIndex = 5
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(304, 412)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.LinkLabelSP)
+        Me.Controls.Add(Me.ToolStrip1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.txb_NumSP)
+        Me.Name = "Form1"
+        Me.Tag = "700; 450"
         Me.Text = "Form1"
+        Me.ToolStrip1.ResumeLayout(False)
+        Me.ToolStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents txb_NumSP As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ToolStrip1 As ToolStrip
+    Friend WithEvents ToolStripDropDownButton1 As ToolStripDropDownButton
+    Friend WithEvents ВыбратьПапкуToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ВыходToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ToolStripDropDownButton2 As ToolStripDropDownButton
+    Friend WithEvents ИзменитьАдресаРабочихПапокToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ToolStripButton2 As ToolStripButton
+    Friend WithEvents LinkLabelSP As LinkLabel
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents FBD As FolderBrowserDialog
 End Class
--- FoldSP/Form1.vb
+++ FoldSP/Form1.vb
@@ -1,3 +1,110 @@
 ﻿Public Class Form1
 
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        LinkLabelSP.Text = ""
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles txb_NumSP.TextChanged
+        If Me.txb_NumSP.Text = "" Then
+            Me.Button1.Enabled = False
+        Else
+            'If IsNumeric(sender.text) Then
+            '    Me.txb_NumSP.Tag = Me.txb_NumSP.Text
+            'Else
+            '    Me.txb_NumSP.Text = Me.txb_NumSP.Tag
+            'End If
+
+            Me.Button1.Enabled = True
+            End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'todo добавить поиск папки по номеру СП
+        'todo добавить обработку нескольких найденных папок
+        LinkLabelSP.Text = txb_NumSP.Text
+        LinkLabelSP.Tag = "E:\Работа\Спецзаказы\!Стандартные\ТП АСН-100-09-Э-03.78 НЖ"
+        'todo создать в параметрах пути папок
+        LinkLabelSP.Text = ПолучениеИмениПапкиWork()
+    End Sub
+
+    Private Sub LinkLabelSP_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabelSP.LinkClicked
+        Shell("explorer.exe " & LinkLabelSP.Tag, vbNormalFocus)
+    End Sub
+
+    Private Sub txb_NumSP_Enter(sender As Object, e As EventArgs) Handles txb_NumSP.Enter
+        If txb_NumSP.Text = "" Then txb_NumSP.Text = "" : Exit Sub
+        'If txb_SP.Text Like "*[!0-9]*" Then txb_SP.Text = txb_SP.Tag Else : txb_SP.Tag = txb_SP.Text
+        'txb_SPFolder.Text = ПоискПапкиWork("\\Localserver\общие документы\Документация СП\Предзаказ\2018")
+        'If txb_SPFolder.Text = "" Then txb_SPFolder.Text = ПолучениеИмениПапкиWork("\\Localserver\общие документы\Документация СП\Предзаказ\2018")
+        'txb_SP.Tag = ""
+    End Sub
+
+    'Private Sub txb_NumSP_KeyUp(sender As Object, e As KeyEventArgs) Handles txb_NumSP.KeyUp
+    '    If txb_NumSP.Text Like "*[!0-9]*" Then txb_NumSP.Text = txb_NumSP.Tag Else txb_NumSP.Tag = txb_NumSP.Text
+    '    '    If Not Char.IsDigit(e.KeyValue) Then e.Handled = True
+    'End Sub
+
+    Private Sub txb_NumSP_KeyDown(sender As Object, e As KeyEventArgs) Handles txb_NumSP.KeyDown
+        If e.KeyData = Keys.Enter Then
+            Exit Sub
+        End If
+
+        If (e.KeyValue >= 48 And e.KeyValue <= 57) Or (e.KeyValue >= 96 And e.KeyValue <= 105) Or e.KeyData = Keys.Delete Or e.KeyData = Keys.Back Then '48, 57, 96, 105
+            e.SuppressKeyPress = False
+        Else
+            e.SuppressKeyPress = True
+        End If
+    End Sub
+    'MsgBox("", , "")
+
+    '======================= ДОП ФУНКЦИИ =============================================
+    '======================= ДОП ФУНКЦИИ =============================================
+    Private Function ПолучениеИмениПапкиWork(Optional ByVal InitialPath As String = "", Optional ByVal Title As String = "Выбор папки") As String
+        ' функция выводит диалоговое окно выбора папки с заголовком Title,
+        ' начиная обзор диска с папки InitialPath
+        ' возвращает полный путь к выбранной папке, или пустую строку в случае отказа от выбора
+        If Title = "" Then Title = "Выберите папку"
+        'Dim PS As String : PS = PathSeparator
+        FBD.RootFolder = Environment.SpecialFolder.ProgramFiles ' Указываем начальную папку(указывать можно только определенные папки(в нашем случае Programm Files))
+        If FBD.ShowDialog = DialogResult.OK Then MsgBox(FBD.SelectedPath) '
+        ПолучениеИмениПапкиWork = FBD.SelectedPath
+        If Not ПолучениеИмениПапкиWork.EndsWith(IO.Path.PathSeparator) Then ПолучениеИмениПапкиWork &= IO.Path.PathSeparator
+
+        'If Not Strings.Right$(ПолучениеИмениПапкиWork, 1) = PS Then ПолучениеИмениПапкиWork = ПолучениеИмениПапкиWork & PS ' если C:\TEMP то C:\TEMP\
+    End Function
+
+    Private Function ExistDir(ByVal DirName As String) As Boolean ' проверка существования папки
+        ExistDir = False 'Считаем, что пока что не существует
+        ExistDir = (Dir(DirName, vbDirectory) <> "") 'Собственно, ответ на интересующий вопрос
+    End Function
+
+    'Private Function ПоискПапкиWork(sFold As String = "") As String
+    '    LinkLabelSP.Text = "Производится поиск папки..."
+    '    Dim FSO, SubFolder, sFolds, tsOut, fFolder, iii
+    '    iii = -1
+    '    FSO = CreateObject("Scripting.FileSystemObject")   'Создаем объект FileSystemObject
+    '    fFolder = FSO.GetFolder(sFold)   'Путь к корневому каталогу
+    '    For Each SubFolder In fFolder.SubFolders   'Цикл по всем подкаталогам
+    '        If InStr(SubFolder.Name, txb_SP.Text) <> 0 Then
+    '            iii = iii + 1 : DoEvents
+    '            If iii = 0 Then ReDim NajdennyePapki(iii) Else ReDim Preserve NajdennyePapki(iii)
+    '            NajdennyePapki(iii) = SubFolder.Name
+    '        End If
+    '    Next
+    '    '   MsgBox (333)
+    '    If iii > -1 Then
+    '        Dim f As Object, S, jjj, Star6ajaData, Star6ijNomer
+    '        Star6ajaData = 0 : Star6ijNomer = 0
+    '        For jjj = 0 To iii
+    '     Set f = FSO.GetFolder(sFold & "\" & NajdennyePapki(jjj))
+    '     S = f.DateCreated : DoEvents
+    '            If S > Star6ajaData Or Star6ajaData = 0 Then
+    '                Star6ajaData = S : Star6ijNomer = jjj
+    '            End If
+    '        Next
+    '        ПоискПапкиWork = NajdennyePapki(Star6ijNomer)
+    '    End If
+    'End Function
+
+
 End Class
